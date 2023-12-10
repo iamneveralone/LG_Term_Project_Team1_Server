65c17d3ddf7cd3ea265305a718a8d0af74d7cf7d
iamneveralone <fieldsawarded@naver.com>
2023-12-11 01:26:15 +0900

    feat : 비디오 시청 기능 구현

---
+++ src/main/java/lg/termproject/dto/LastPlaytimeDto.java
@@ -0,0 +1,12 @@
+package lg.termproject.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LastPlaytimeDto {
+    private int lastPlaytime;
+}
